23f25f0eac5d6e97cbc755c88dd6e786bf7adf05
Ayush <ayush642001@gmail.com>
2026-08-18 16:11:19 +0530

    chore: disable Astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ayush190511.github.io',
+  devToolbar: {
+    enabled: false,
+  },
 });
+
